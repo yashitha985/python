ff99cfc35b8de78feaa13c1751720f194d22a869
M.Yashitha <52398907+yashitha985@users.noreply.github.com>
2019-07-18 22:26:11 +0530

    Create hunter6.py

---
+++ hunter6.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=list(map(int,input().split()))
+c=[]
+m=0
+for i in b:
+  if (b.count(i)>1):
+    if i not in c:
+      c.append(i)
+if (len(c)==0):
+  print('unique')
+else:
+  print(c[0]
